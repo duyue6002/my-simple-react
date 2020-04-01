ea076b2bf6ed01da621e572f8aa06671efb8d4f8
duyue6002 <duyue6002@gmail.com>
2020-04-01 18:07:03 +0800

    react & reactdom

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from "./react";
+import ReactDOM from "./reactdom";
+
+// Notes: 为什么看上去没用到React还需要调用呢？
+// JSX 语法糖会把“DOM标签”转化成虚拟DOM对象
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello World</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
+
+setInterval(tick, 1000);
